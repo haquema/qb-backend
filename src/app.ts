691cd8c771934892a58938d1b6f9687208bbc54f
Aziz <113197790+haquema@users.noreply.github.com>
2024-11-11 17:46:01 +0000

    clerk authentication added

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,14 @@
+import 'dotenv/config';
 import express from 'express';
 import { clerkMiddleware } from '@clerk/express';
+
 import prisma from '../utils/prisma';
 
 const app = express()
 
-// initialise Clerk
-app.use(clerkMiddleware())
-
-app.use(express.json())
+// Middleware
+app.use(clerkMiddleware()) // Clerk authentication
+app.use(express.json()) // Parse JSON correctly
 
 app.get('/users', async (req, res) => {
   const users = await prisma.user.findMany()
